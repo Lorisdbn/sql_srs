45749bdab69adfb220027fed19b2e1ed930325b6
Loris Durbano <loris.durbano@gmail.com>
2026-02-07 18:49:36 +0100

    ajout de l'app et des requirements dans repo

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import duckdb
+
+
+st.write("Bonjour")
+
+
+data = {
+    "a": [1, 2, 3],
+    "b": [4, 5, 6],
+    "c": [7, 8, 9],
+    "d": [10, 11, 12]
+}
+
+df=pd.DataFrame(data)
+
+tab_1, tab_2 = st.tabs(["1er onglet", "2ème onglet"])
+
+with tab_1:
+    sql_query = st.text_area(label="Input text", key="input1")
+    result = duckdb.query(sql_query).df()
+    st.write(f"you entered the following query : {sql_query}")
+    st.dataframe(result)
+
+
+with tab_2:
+    input_text = st.text_area(label="Input text", key="input2")
+    st.write(input_text)
